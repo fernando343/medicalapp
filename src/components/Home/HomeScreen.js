407c4fe3870b7cf29b8bd36316382f1b07355e9e
Fernando Lopez <fernandojalope343@gmail.com>
2021-04-01 13:45:10 -0400

    specialist in your area

--- src/components/Home/HomeScreen.js
+++ src/components/Home/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Text } from 'react-native';
-import { View } from 'react-native';
+import { View, FlatList, StyleSheet } from 'react-native';
 import BackgroudHeader from '../../layout/BackgroundHeader';
 import DoctorItem from '../Doctors/DoctorItem';
 
@@ -10,10 +10,25 @@ const HomeScreen = () => {
         <View>
             <BackgroudHeader />
             <View>
-                <DoctorItem/>
+                <Text style={styles.areaText}>Specialist in your area</Text>
+                <FlatList
+                    data={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }].slice(0, 1)}
+                    renderItem={() =>
+                        <DoctorItem />
+                    }
+                />
             </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    areaText:{
+        fontWeight: "bold",
+        fontSize: 20,
+        marginLeft: 20,
+        marginTop: 15
+    }
+})
 
 export default HomeScreen;
